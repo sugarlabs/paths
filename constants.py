fc5a031aee34dff95f15a737c2f74245932c74a2
Walter Bender <walter.bender@gmail.com>
2011-03-22 03:40:51 -0400

    using common constants file

---
+++ constants.py
@@ -0,0 +1,28 @@
+#Copyright (c) 2011 Walter Bender
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
+NORTH = 0
+EAST = NORTH + 1
+SOUTH = EAST + 1
+WEST = SOUTH + 1
+
+ROW = 8
+COL = 8
+
+HIDE = 0
+BOARD = 1
+GRID = 2
+CARDS = 3
+OVERLAY = 4
+
+CARD_WIDTH = 55
+CARD_HEIGHT = 55
