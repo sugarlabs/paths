b1bf90471926ff7dc4a1d836b9280c5e7c479748
Walter Bender <walter.bender@gmail.com>
2011-10-28 08:03:36 -0400

    fixed address in license

--- tile.py
+++ tile.py
@@ -1,14 +1,14 @@
-#Copyright (c) 2009-11 Walter Bender
+#Copyright (c) 2011 Walter Bender
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-# Boston, MA 02111-1307, USA.
+# You should have received a copy of the GNU General Public License
+# along with this library; if not, write to the Free Software
+# Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
+
 
 import gtk
 from constants import NORTH, EAST, SOUTH, WEST, HIDE, TILES
